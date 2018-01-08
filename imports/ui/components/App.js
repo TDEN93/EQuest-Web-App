a56668a6853a375df7e321fb726503a812f6bbc9
Tray Denney <denney.tray@gmail.com>
2018-01-08 13:17:15 -0600

    Live-Website

--- imports/ui/components/App.js
+++ imports/ui/components/App.js
@@ -7,42 +7,33 @@ const FontAwesome = require('react-fontawesome');
 export default class App extends Component {
 
   render() {
-
     return (
       <div className="container">
 
         <div className="navigation-grid">
             <i class="menu-icon fa fa-bars fa-2x" aria-hidden="true"></i>
-
-
         </div>
 
         <div className="content-grid">
             <img className="mobile-logo" src="/logo-full.svg" />
+            <img className="desktop-logo" src="/beakers.svg" />
             <h1>THE FUTURE</h1>
             <span>OF GAMING INTELLIGENCE IS HERE</span>
 
             <section>
-              <span className="desc-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              <span className="main-copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                Hic rem tempora sequi ut, culpa harum illum nam?
                Illum voluptates accusamus doloribus debitis autem aliquam
                dolores.</span>
             </section>
 
             <button className="demo-button">Request a demo</button>
-
-
         </div>
 
         <div className="footer-grid">
             <img className="wave" src="/mobile-waves.svg" />
         </div>
-
-
       </div>
-
     );
-
   }
-
 }
