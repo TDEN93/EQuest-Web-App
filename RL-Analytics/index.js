6e9cd9b2348f51e220a270915346b9a92f6c0e9c
Tray Denney <denney.tray@gmail.com>
2018-03-11 06:06:31 -0500

    Added VEH links
    Can now link between player actor_ids and vehicle actor_ids

--- RL-Analytics/index.js
+++ RL-Analytics/index.js
@@ -1,6 +1,19 @@
+/*
+Project: Rocket League Replay Analysis
+Author: Tray Denney, Brandon VanCamp
+Description: Pulls player and ball information from Rocket League replay files.
+Useful Resources: 
+- http://ignitersworld.com/lab/jsonQ.html Library used to traverse JSON files.
+- https://www.dropbox.com/sh/152ay45pzsu9k94/AABmh1YcAZ5XuivYdR7qiOJLa?dl=0 Replay parser and decoder created by Tray Denney.
+- https://www.dropbox.com/sh/iz1ccenw11aqnzp/AAABerEXLdt29UxLKUBXCOhsa?dl=0 Small list of sample replay files.
+- https://cdn.discordapp.com/attachments/379071324741697538/383875441867620352/mov_data.png JSON traversal path.
+- https://www.npmjs.com/package/jsonfile Library used to load JSON file.
+- https://github.com/TDEN93/EQuest-Web-App/tree/master/RL-Analytics Github page.
+*/
+
 const jsonQ = require('jsonq');
 const jsonFile = require('jsonfile');
-const file = 'replays/test.replay.json';
+const file = 'replays/input.replay.json';
 
 let jsonObj = jsonFile.readFileSync(file);
 let replayJson = jsonQ(jsonObj);
@@ -42,8 +55,7 @@ vehicle_ID_actor.each(function(index,path,value){
 
 veh_ids.push(uniqueID(veh_id_array));
 
-// console.log(uniqueID(veh_ids)[0][0]);
-// console.log(uniqueID(player_id_array)[0]);
+
 // -----------------------------------------------------------------------------------------------
 
 
@@ -52,19 +64,21 @@ let actor_ids = replayJson.find('rigid_body_state_attribute_value', function(){
 }).parent().parent().parent().parent().parent();
 
 actor_ids.each(function(index,path,values){
-    // Grab all player attributes
-    // for(let i = 0; i < veh_ids.length; i++){
-    //     if(values.actor_id.value == veh_ids[i][0]){
-    //         console.log(values.value.updated_replication_value[i].value.rigid_body_state_attribute_value);
-    //     }
-    // }
+    for(let i = 0; i < veh_ids[0].length; i++){
+        if(values.actor_id.value == veh_ids[0][i]){
+            
+            for(let j = 0; j < values.value.updated_replication_value.length; j++){
+                if(values.value.updated_replication_value[j].value.rigid_body_state_attribute_value != undefined){
+                    // console.log("Actor_ID " + values.actor_id.value);
+                    // console.log("Veh ID " + veh_ids[0][i]);
+                    player_data.push(values.value.updated_replication_value[j].value.rigid_body_state_attribute_value);
+                    // Need to clean up and sort information.
+                }
+            }
+            
 
-    for(let i = 0; i < veh_ids.length; i++){
-        if(values.actor_id.value == veh_ids[0][0]){
-            player_data.push(values.value.updated_replication_value[i].value.rigid_body_state_attribute_value);
-            // WHY WILL THIS NOT ALLOW ME TO ACCESS LOCATION DATA
         }
     }
 })
 
-console.log(player_data);
+console.log(player_data[5]);
