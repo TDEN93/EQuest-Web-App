54ad371951cfb04ce0ca3398efab299fd143cb7b
Brandon VanCamp <brandon@overtimewebdev.com>
2018-04-12 08:54:18 -0500

    Nav Fix
    Mobile nav now collapes to show nav for mobile view.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,31 +3,44 @@ import logo from './assets/images/logo-full.svg';
 import { NavLink } from 'react-router-dom';
 import { // BS import
   Nav,
-  NavItem
+  NavItem,
+  NavDropdown,
+  MenuItem,
+  Col,
+  Glyphicon,
+  Grid,
+  Navbar,
+  Row
 } from 'react-bootstrap';
 import AnchorLink from './Link';
-
 const SiteNav = (props) => {
   const Links = props.navPage.navNames.map((link, index) => {
-    return <AnchorLink key={index} name={link} />
+    return <AnchorLink key={index} name={link}/>
   });
+
   function handleSelect(selectedKey) {
     alert(`selected ${selectedKey}`);
   }
-  return (
-    <nav className="navbar navbar-expand-lg ">
-      <NavLink to='/' className="navbar-brand">
-        <img src={logo} alt="logo" height="50" />
-      </NavLink>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <Nav bsClass='navBar' className='navbar-nav' bsStyle="pills" activeKey={1} onSelect={handleSelect}>
+  return(<Grid fluid={true}>
+      <Row>
+        <Col xs={12}>
+    <Navbar inverse="inverse" collapseOnSelect="collapseOnSelect">
+      <Navbar.Header>
+        <Navbar.Brand>
+          <NavLink to='/'>
+            <img src={logo} alt="logo" height="50"/>
+          </NavLink>
+        </Navbar.Brand>
+        <Navbar.Toggle/>
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight="pullRight">
           {Links}
         </Nav>
-      </div>
-    </nav>
-  );
+      </Navbar.Collapse>
+    </Navbar>
+    </Col>
+    </Row>
+  </Grid>);
 }
 export default SiteNav;
