3ba5d9111d848cdd86a8ed4a071c7a48561fc968
Tray Denney <denney.tray@gmail.com>
2018-04-13 10:56:16 -0500

    Home Fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,66 +1,44 @@
 import React from 'react';
-
 import { // BS import
-  Grid,
-  Row,
-  Col,
-  Clearfix
-} from 'react-bootstrap';
-
-import 'font-awesome/css/font-awesome.min.css';
-import replayIcon from './assets/images/icons-home/replay.png';
-import statsIcon from './assets/images/icons-home/stats.png';
-import tacticalIcon from './assets/images/icons-home/tactical.png';
-
-
-import { Png } from './modules/IconList'; // Import icons with Figcaption.
+	Grid,
+	Row,
+	Col,
+	Clearfix
+}
+from 'react-bootstrap';
+import { Png, Svg } from './modules/IconList'; // Import icons with Figcaption.
 const Home = (props) => {
 	const page = props.homePage;
-	
+	const svgs = page.svgs.map((svg, i) => { // Map svg files and append.
+		return <Svg key={i} src={svg}/>
+	});
 	const pngs = page.pngs.map((pngs, i) => { // Map png icons files  with figcaption and append.
 		return <Png key={i} src={pngs} caption={page.figcaption[i]}/>
-});
-  return (
-    <header id="home-section">
-      <Grid fluid={true}>
-        <Row className="home-title">
-          <Col className="social-media-container">
-            <ul className="social-media">
-              <li><a href="/"><i className="fa fa-twitter fa-2x"></i></a></li>
-              <li><a href="/"><i className="fa fa-facebook-square fa-2x"></i></a></li>
-              <li><a href="/"><i className="fa fa-google-plus fa-2x"></i></a></li>
-              <li><a href="/"><i className="fa fa-instagram fa-2x"></i></a></li>
-            </ul>
-          </Col>
-          <Col xs={12} md={12} lg={12} className="main-content">
-            <h1>
-              <span>{page.title}</span>{page.titleTopColored}{page.titleTopAfter}
-            </h1>
-            <p>{page.titleTag}<span>{page.titleTagColored}</span>{page.titleTagAfter}</p>
-          </Col>
-          <Col xs={12} md={12}>
-            <div className="service-icons-container">
+	});
+	return(<header id="home-section">
+    <Grid fluid={true}>
+      <Row className="home-title">
+        <Col className="social-media-container">
+          <ul className="social-media">
+            {/* Append svg icons to page */}
+            {svgs}
+          </ul>
+        </Col>
+        <Col xs={12} md={12} lg={12} className="main-content">
+          <h1>
+            <span>{page.title}</span>{page.titleTopColored}{page.titleTopAfter}
+          </h1>
+          <p>{page.titleTag}<span>{page.titleTagColored}</span>{page.titleTagAfter}</p>
+        </Col>
+        <Col xs={12} md={12}>
+          <div className="service-icons-container">
+            {/* Append all png icons and figcaptions to page */}
             {pngs}
-              {/* <a href="/"><figure className="service-item">
-                <img src={replayIcon} alt="replay-icon" height="80" />
-                <figcaption className="caption">{page.replayService}</figcaption>
-              </figure></a>
-
-              <a href="/"><figure className="service-item">
-                <img src={statsIcon} alt="replay-icon" height="80" />
-                <figcaption className="caption">{page.overallService}</figcaption>
-              </figure></a>
-
-              <a href="/"><figure className="service-item">
-                <img src={tacticalIcon} alt="replay-icon" height="80" />
-                <figcaption className="caption">{page.tacticalService}</figcaption>
-              </figure></a> */}
-            </div>
-          </Col>
-          <div className="corner-ribbon bottom-right sticky orange shadow">Beta</div>
-        </Row>
-      </Grid>
-    </header>
-  );
+          </div>
+        </Col>
+        <div className="corner-ribbon bottom-right sticky orange shadow">Beta</div>
+      </Row>
+    </Grid>
+  </header>);
 }
 export default Home;
