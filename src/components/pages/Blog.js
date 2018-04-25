cc8049fd580b341dabd0f211ffbea08beb62e889
Tray Denney <denney.tray@gmail.com>
2018-04-24 23:59:18 -0500

    Blog Update

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -16,6 +16,10 @@ const Blog = (props) => {
             <Grid fluid={true} className="blog-container">
                 <Row>
                     <Col xs={12} md={12} lg={12} className="blogCol">
+                        <div className="blog-title">
+                            <h1>Latest News & Research</h1>
+                            <p>Stay up to date with the latest in esports news and research.</p>
+                        </div>
                         <div className="tabs">
                             <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
                                 <Tab eventKey={1} title="Most Recent">
@@ -27,51 +31,55 @@ const Blog = (props) => {
                                 <Tab eventKey={3} title="Rocket League">
                                     
                                 </Tab>
+                                
                             </Tabs>
+                            
                         </div>
                         <div className="articleContainer">
                             <div className="card">
-                                <img src={blogImage} alt="Avatar" className="card-image"></img>
                                 <div className="container">
-                                    <h4><b>John Doe</b></h4>
-                                    <p>Architect & Engineer</p>
+                                    <img src={blogImage} alt="Avatar" className="card-image"></img>
+                                    <div className="card-content">
+                                        <p className="card-category">Category: <span><a href="#">Rocket League</a></span></p>
+                                        <h4 className="card-title"><b>Rocket League Finally Fixes Servers</b></h4>
+                                        <p className="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed velit dignissim, placerat augue at, sagittis eros. Mauris eleifend lacinia dapibus.</p>
+                                        <div className="card-info">
+                                            by John Smith - April 24, 2018 - 999 comments
+                                        </div>
+                                    </div> 
                                 </div>
                             </div>
+
                             <div className="card">
-                                <img src={blogImage} alt="Avatar" className="card-image"></img>
                                 <div className="container">
-                                    <h4><b>John Doe</b></h4>
-                                    <p>Architect & Engineer</p>
+                                    <img src={blogImage} alt="Avatar" className="card-image"></img>
+                                    <div className="card-content">
+                                        <p className="card-category">Category: <span><a href="#">Rocket League</a></span></p>
+                                        <h4 className="card-title"><b>Rocket League Finally Fixes Servers</b></h4>
+                                        <p className="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed velit dignissim, placerat augue at, sagittis eros. Mauris eleifend lacinia dapibus.</p>
+                                        <div className="card-info">
+                                            by John Smith - April 24, 2018 - 999 comments
+                                        </div>
+                                    </div> 
                                 </div>
                             </div>
+
                             <div className="card">
-                                <img src={blogImage} alt="Avatar" className="card-image"></img>
                                 <div className="container">
-                                    <h4><b>John Doe</b></h4>
-                                    <p>Architect & Engineer</p>
+                                    <img src={blogImage} alt="Avatar" className="card-image"></img>
+                                    <div className="card-content">
+                                        <p className="card-category">Category: <span><a href="#">Rocket League</a></span></p>
+                                        <h4 className="card-title"><b>Rocket League Finally Fixes Servers</b></h4>
+                                        <p className="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed velit dignissim, placerat augue at, sagittis eros. Mauris eleifend lacinia dapibus.</p>
+                                        <div className="card-info">
+                                            by John Smith - April 24, 2018 - 999 comments
+                                        </div>
+                                    </div> 
                                 </div>
                             </div>
-                            <div className="card">
-                                <img src={blogImage} alt="Avatar" className="card-image"></img>
-                                <div className="container">
-                                    <h4><b>John Doe</b></h4>
-                                    <p>Architect & Engineer</p>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <img src={blogImage} alt="Avatar" className="card-image"></img>
-                                <div className="container">
-                                    <h4><b>John Doe</b></h4>
-                                    <p>Architect & Engineer</p>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <img src={blogImage} alt="Avatar" className="card-image"></img>
-                                <div className="container">
-                                    <h4><b>John Doe</b></h4>
-                                    <p>Architect & Engineer</p>
-                                </div>
-                            </div>           
+
+                            
+                              
                         </div>
                     </Col>
                 </Row>
